70e8251851bcf99b58d8a95e9bd764b1de8303d1
Schneggera <37636221+Schneggera@users.noreply.github.com>
2020-12-22 17:51:09 +0100

    added comment conf.py

--- conf.py
+++ conf.py
@@ -61,6 +61,7 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# Options for theme
 html_theme_options = {
     'navigation_depth': 2
 }
